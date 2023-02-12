94542ab6c3e884957883140dc188b5b4fa1c274b
adagio <andrea.dgioacchino@gmail.com>
2023-02-12 18:09:25 +0100

    added sliding window computation and tests

--- src/DSForces.jl
+++ src/DSForces.jl
@@ -62,17 +62,111 @@ function FindLongestComplementarySegment(seq::Union{LongDNA{4}, LongSubSeq{DNAAl
     end
 end
 
-
-function ComputeDSForce(seq::Union{LongDNA{4}, LongSubSeq{DNAAlphabet{4}}})
-    CSL = FindLongestComplementarySegment(seq)
-    α = ComputeAlpha(seq)
-    return CompSegmLenToDSForce(CSL, α, length(seq))
+function FindLongestComplementarySegmentLast(seq::Union{LongDNA{4}, LongSubSeq{DNAAlphabet{4}}})
+    pattern_seq = LongDNA{4}(replace(String(seq), "A" => "T", "C" => "G", "G" => "Y", "T"=>"R"))
+    L = length(seq)
+    maxL = 2
+    start_segm_pos = L - 2
+    @inbounds for k in maxL:L
+        if !(occursin(ExactSearchQuery(reverse(pattern_seq[L-k:L]), iscompatible), @view seq[1:L-k-1]))
+            if k+1 >= maxL
+                maxL = k + 1
+                start_segm_pos = L-k
+            end
+            break
+        end
+    end
+    eqs = ExactSearchQuery(reverse(pattern_seq[start_segm_pos+1:L]), iscompatible)
+    if isnothing(findfirst(eqs, seq[1:start_segm_pos]))
+        return L:L, L:L
+    else
+        return findfirst(eqs, seq[1:start_segm_pos]), start_segm_pos+1:L
+    end
 end
 
-function ComputeDSForce(seq::AbstractString)
+function FindLongestComplementarySegmentSliding(seq::LongDNA{4}, 
+    sliding_window_length::Int=min(length(seq), 3000))
+    LCSLs = Int[]
+    alphas = Float64[]
+    range1s = UnitRange{Int64}[]
+    range2s = UnitRange{Int64}[]
+    global_pos = 1
+    while global_pos <= length(seq)-sliding_window_length+1
+        sliding_seq = seq[global_pos:global_pos+sliding_window_length-1]        
+        nt_usage = countmap(sliding_seq)
+        ks_ntu = keys(nt_usage)
+        if sum([nt in ks_ntu ? nt_usage[nt] : 0 for nt in allowed_alphabet]) != sliding_window_length
+            push!(LCSLs, 0)
+            push!(alphas, 0)
+            push!(range1s, 1:1)
+            push!(range2s, 1:1)
+            global_pos += 1
+            continue
+        end
+        t_alpha = sum([((p[1] in ks_ntu) & (p[2] in ks_ntu)) ? nt_usage[p[1]] * nt_usage[p[2]] / sliding_window_length^2 : 0 for p in allowed_pairs])
+        push!(alphas, t_alpha)
+        if global_pos == 1
+            t_range1, t_range2 = FindLongestComplementarySegment(sliding_seq, true)
+            push!(range1s, t_range1 .+ (global_pos-1))
+            push!(range2s, t_range2 .+ (global_pos-1))
+        else
+            pre_range1 = range1s[end]
+            if pre_range1[1] < global_pos
+                t_range1, t_range2 = FindLongestComplementarySegment(sliding_seq, true)
+                push!(range1s, t_range1 .+ (global_pos-1))
+                push!(range2s, t_range2 .+ (global_pos-1))
+            else
+                pre_length = LCSLs[end]
+                lastpos_range1, lastpos_range2 = FindLongestComplementarySegmentLast(sliding_seq)
+                if length(lastpos_range1) > pre_length
+                    t_range1, t_range2 = lastpos_range1, lastpos_range2
+                    push!(range1s, lastpos_range1 .+ (global_pos-1))
+                    push!(range2s, lastpos_range2 .+ (global_pos-1))
+                else
+                    push!(range1s, range1s[end])
+                    push!(range2s, range2s[end])
+                end
+            end
+        end
+        push!(LCSLs, length(range1s[end]))
+        global_pos += 1
+    end    
+    return LCSLs, alphas, range1s, range2s
+end
+
+
+function ComputeDSForce(seq::Union{LongDNA{4}, LongSubSeq{DNAAlphabet{4}}};
+                        return_LCS_positions::Bool=false, 
+                        sliding_window_length::Union{Int, Missing}=missing)
+    if ismissing(sliding_window_length)
+        α = ComputeAlpha(seq)
+        if return_LCS_positions
+            rangeA, rangeB = FindLongestComplementarySegment(seq, true)
+            CSL = length(rangeA)
+            return CompSegmLenToDSForce(CSL, α, length(seq)), rangeA, rangeB
+        else
+            CSL = FindLongestComplementarySegment(seq)
+            return CompSegmLenToDSForce(CSL, α, length(seq))
+        end
+    else
+        CSLs, alphas, range1s, range2s = FindLongestComplementarySegmentSliding(seq, sliding_window_length)
+        DSFs = [CompSegmLenToDSForce(x, y, sliding_window_length) for (x,y) in zip(CSLs, alphas)]
+        if return_LCS_positions
+            return DSFs, range1s, range2s
+        else
+            return DSFs
+        end
+    end
+end
+
+function ComputeDSForce(seq::AbstractString;
+                        return_LCS_positions::Bool=false, 
+                        sliding_window_length::Union{Int, Missing}=missing)
     # here I should check that no non-ACTG symbol is present
     DNA_seq = LongDNA{4}(seq)
-    return ComputeDSForce(DNA_seq)
+    return ComputeDSForce(DNA_seq; return_LCS_positions=return_LCS_positions, 
+                            sliding_window_length=sliding_window_length)
 end
+
 
 end
